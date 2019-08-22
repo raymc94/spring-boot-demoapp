b3a51b502710f169324967e930319d88b71dce25
Domingo Gallardo <domingo.gallardo@ua.es>
2019-08-22 09:57:20 +0200

    Primer commit

---
+++ src/main/java/demoapp/controller/SaludoControllerPlantilla.java
@@ -0,0 +1,26 @@
+package demoapp.controller;
+
+import demoapp.service.SaludoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class SaludoControllerPlantilla {
+
+    private final SaludoService service;
+
+    @Autowired
+    public SaludoControllerPlantilla(SaludoService service) {
+        this.service = service;
+    }
+
+    @RequestMapping("/saludoplantilla/{nombre}")
+    public String saludo(@PathVariable(value="nombre") String nombre, Model model) {
+        model.addAttribute("mensaje", service.saluda(nombre));
+        return "saludo";
+    }
+}

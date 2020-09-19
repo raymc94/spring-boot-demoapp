2a6cbc074f84f4869fc88bae4cbf147a2b697103
Domingo Gallardo <domingo.gallardo@ua.es>
2020-09-19 11:18:09 +0200

    @AutoConfigureMockMvc en todos los tests web

--- src/test/java/demoapp/WebMockTest.java
+++ src/test/java/demoapp/WebMockTest.java
@@ -1,12 +1,12 @@
 package demoapp;
 
 
-import demoapp.controller.SaludoController;
 import demoapp.service.SaludoService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -19,7 +19,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(SaludoController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class WebMockTest {
 
     @Autowired

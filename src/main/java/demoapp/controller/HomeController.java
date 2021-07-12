05903718abc3e0b6fe64031ee225e986b41a1afa
Ray <raymc94@gmail.com>
2021-07-12 19:49:15 +0200

    issue 8 - add acerca de y release 1.0.0

--- src/main/java/demoapp/controller/HomeController.java
+++ src/main/java/demoapp/controller/HomeController.java
@@ -1,17 +1,40 @@
 package demoapp.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import pojo.UserData;
+import demoapp.model.Usuario;
+import demoapp.service.UsuarioService;
 
 @Controller
 public class HomeController {
 
+	@Autowired
+	UsuarioService usuarioService;
+	
     @GetMapping("/")
     public String greeting() {
     	return "redirect:/login";
+    }
+    
+    @GetMapping("/about")
+    public String loginForm(Model model, HttpSession session) {
+		
+		Usuario usuario = null;
+		
+		if(session.getAttribute("idUsuarioLogeado") != null) {
+			Long idUsuario = (Long )session.getAttribute("idUsuarioLogeado");
+			
+			usuario = usuarioService.findById(idUsuario);
+		}
+		
+        model.addAttribute("usuario", usuario);
+		
+        return "acercaDe";
     }
     
 }
